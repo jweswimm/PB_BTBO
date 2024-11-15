262e6e22499d7e8293da7a410ab9b5368a52feb4
Joe Wimmergren <joewimmergren@Olivias-iMac.lan>
2024-11-15 16:42:09 -0600

    Fixed up a quick current weather api call

--- src/Telemetry/weatherScraper.py
+++ src/Telemetry/weatherScraper.py
@@ -1,18 +1,40 @@
 import requests
+import datetime as dt
 from APIkey import tomorrowio_API_KEY
+import IPython
 
 class weatherScraper:
     def __init__(self):
         print('Testing')
+        self._apikey = tomorrowio_API_KEY
 
-        url = "https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=a7nOway2UAAoysDeJU1ooToEqoo7d0e1"
+    def _getCurrentWeather(self):
+        realTimeUrl = "https://api.tomorrow.io/v4/weather/realtime"
+        # lat lon pair for Duluth MN
+        location = [46.7867, 92.1005]
 
-        headers = {"accept": "application/json"}
+        fields = ["temperature", "precipitationProbability", "precipitationType", "windSpeed", "windDirection", "humidity", "cloudCover"]
+        units = "imperial"
 
-        response = requests.get(url, headers=headers)
+        # Construct the query parameters
+        params = {
+            "location": f"{location[0]},{location[1]}",
+            "fields": ",".join(fields),
+            "units": units,
+            "apikey": self._apikey
+        }
 
-        print(response.text)
-    
+        # Make the GET request
+        response = requests.get(realTimeUrl, params=params)
+        
+        if response.status_code == 200:
+            data = response.json()
+            IPython.embed()
+            data = data["data"]
+            print(data)
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
 
 if __name__ == "__main__":
-    weatherScraper()
+    weatherobj = weatherScraper()
+    weatherobj._getCurrentWeather()
