78e5cde07b79be18061b644021da914ea511b7fe
Joe Wimmergren <joewimmergren@Olivias-iMac.lan>
2024-11-16 11:08:17 -0600

    RealTimeWeather changes | Asynch requests and streamlining API calls

--- src/Telemetry/weatherScraper.py
+++ src/Telemetry/weatherScraper.py
@@ -1,40 +1,45 @@
-import requests
+import aiohttp
+import asyncio
 import datetime as dt
 from APIkey import tomorrowio_API_KEY
 import IPython
 
-class weatherScraper:
-    def __init__(self):
-        print('Testing')
+class RealTimeWeather:
+    def __init__(self, location=[46.7867, 92.1005], fields= ["temperature", "windSpeed", "windDirection"]):
+
+        #Initialize Params
         self._apikey = tomorrowio_API_KEY
 
-    def _getCurrentWeather(self):
-        realTimeUrl = "https://api.tomorrow.io/v4/weather/realtime"
-        # lat lon pair for Duluth MN
-        location = [46.7867, 92.1005]
+        # Define the URL for the real-time weather data 
+        self._realTimeUrl = "https://api.tomorrow.io/v4/weather/realtime"
 
-        fields = ["temperature", "precipitationProbability", "precipitationType", "windSpeed", "windDirection", "humidity", "cloudCover"]
-        units = "imperial"
+        #fields = ["temperature", "precipitationProbability", "precipitationType", "windSpeed", "windDirection", "humidity", "cloudCover"]
+        self._units = "imperial"
 
         # Construct the query parameters
-        params = {
+        self._params = {
             "location": f"{location[0]},{location[1]}",
             "fields": ",".join(fields),
-            "units": units,
+            "units": self._units,
             "apikey": self._apikey
         }
 
-        # Make the GET request
-        response = requests.get(realTimeUrl, params=params)
-        
-        if response.status_code == 200:
-            data = response.json()
-            IPython.embed()
-            data = data["data"]
-            print(data)
-        else:
-            print(f"Error: {response.status_code}, {response.text}")
+    async def _getCurrentWeather(self):
+
+        #Use aiohttp to make the asynchronous request
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self._realTimeUrl, params=self._params) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    data = data["data"]
+                    print(data)
+                else:
+                    print(f"Error: {response.status}, {await response.text()}")
 
 if __name__ == "__main__":
-    weatherobj = weatherScraper()
-    weatherobj._getCurrentWeather()
+
+    #Instantiate the RealTimeWeather class
+    weather = RealTimeWeather()
+
+    #Run the asynchronous function
+    asyncio.run(weather._getCurrentWeather())
